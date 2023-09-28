85ef49bfecb6e479e91383f0769b696ca3f83680
yrodr <y.rodr1gues0811@outlook.com>
2023-09-28 15:35:19 -0300

    print possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -46,16 +46,30 @@ public class UI {
         for (int i = 0; i < chessPieces.length; i++) {
             System.out.print((8-i) + " ");
             for (int j = 0; j < chessPieces.length; j++) {
-                printPiece(chessPieces[i][j]);
+                printPiece(chessPieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece piece){
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8-i) + " ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean background){
+        if (background) {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
@@ -67,4 +81,5 @@ public class UI {
         }
         System.out.print(" ");
     }
+
 }
